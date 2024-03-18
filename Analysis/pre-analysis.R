2ef20ce35193db27c6f6d512dd44f1d49f0d6783
epyc <kanggle@outlook.com>
2024-03-18 12:22:31 +0800

    update for AU

--- Analysis/pre-analysis.R
+++ Analysis/pre-analysis.R
@@ -6,7 +6,10 @@ df$Date <- as.Date(df$Date, format = "%Y-%m-%d")
 df$Cases <- as.integer(df$Cases)
 
 ggplot(df)+
-     geom_line(mapping = aes(x = Date, y = Cases, color = Country, group = Country))
+     geom_line(mapping = aes(x = Date, y = Cases, color = Country, group = Country))+
+     facet_wrap(~Country, scales = "free_y")
 
 
 ggsave("../preview.png", width = 10, height = 5, dpi = 300)
+
+# source("./pre-analysis.R")
